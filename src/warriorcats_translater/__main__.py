d4179a6de2718caffee1b3f026f27165cda35132
diegow <diegow876@outlook.com>
2025-12-31 21:27:49 +0800

    do notes: exit codes,__main__.py usage clean,cli.py exception handling,metadata in __init__.py

--- src/warriorcats_translater/__main__.py
+++ src/warriorcats_translater/__main__.py
@@ -1,1 +1,24 @@
-# pyproject标记的入口点
+'''
+pyproject标记的入口点
+
+主入口点的逻辑应该简单，只负责标记入口
+'''
+import sys
+from .cli import main
+
+if __name__ == "__main__":
+    sys.exit(main())  # 只是转发调用
+    '''
+    为何使用sys.exit？
+    接受cli.py的退出码，返回退出码
+    例：a && b这样的脚本逻辑就由退出码决定执行逻辑（a exit 0 -> b/a exit 1 -> x）
+
+    此处用例：
+    cli.py/main: return 1 # 此处只是“码”数据
+    __main__.py：sys.exit(1) # 真实退出码
+
+    异常处理：
+    不应该直接raise（出现异常python解释器会以 1 退出。程序也会，但是程序不会按照显式指定的退出码退出）
+    
+    所以：在cli.py的顶层（raise最终传播到的地方），使用以上正确方式打印异常，返回退出“码”
+    '''
--- src/warriorcats_translater/cli.py
+++ src/warriorcats_translater/cli.py
@@ -1,1 +1,14 @@
-# 真正的能够的cli入口
+'''
+真正的能够的cli入口
+
+分离__main__入口点和cli入口点优势：
+1. 清晰的关注点分离
+2. 便于作为正常包时被导入
+3. 便于专注扩展cli相关功能（比如子命令）
+4. 便于调试
+'''
+def main():
+    pass
+
+if __name__ == "__main__":
+    main() # 允许直接运行 python -m warriorcats_translater.cli
