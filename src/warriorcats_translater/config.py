a9720752581a9d5f6faceec1e1551e2e6bf5de6c
diegow <diegow876@outlook.com>
2026-01-01 15:01:05 +0800

    finished developing load_config

---
+++ src/warriorcats_translater/config.py
@@ -0,0 +1,30 @@
+'''
+此模块用于读写config和进行校验
+'''
+from pydantic import Field,ValidationError
+from pydantic_settings import BaseSettings,SettingsConfigDict
+import json
+import os
+
+class Config(BaseSettings):
+    api_key: str = Field(description="用户的api密钥")
+
+    model_config = SettingsConfigDict(strict=True,env_file="../../.env",env_file_encoding="utf-8") # 警告：使用了本文件到.env的相对路径
+
+def load_config() -> dict:
+    CWD: str = os.getcwd() # 获取用户运行工具的目录
+
+    try:
+        with open(os.path.join(CWD,"config.json")) as f:
+            metadata: dict = json.load(f) # 获取json配置文件数据
+    except FileNotFoundError:
+        print("未发现config.json")
+        raise
+
+    try:
+        config: dict = Config.model_validate(metadata).model_dump() # 校验之后的配置文件结果
+    except ValidationError as e:
+        print(f"配置文件选项错误:{e}")
+        raise
+
+    return config
